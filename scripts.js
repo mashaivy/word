facc7ce316ec639fad1f555851c950c6be4e0bc2
Student <example@example.com>
2019-08-20 16:54:47 -0700

    commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+$(document).ready (function(){
+  $("form#sentence").submit(function(event){
+event.preventDefault();
+
+var wovels =["a","e","i","o", "u"];
+var userInput =$("input#anything").val();
+
+var splitInput = userInput.split("");
+for (var i = 0; i < anything.length; i += 1) {
+  if (splitInput[i] === "a" || splitInput === "e" || splitInput === "i"||splitInput === "o"|| splitinput==="u"){
+    splitInput[i] = "-";
+
+  })
+})
